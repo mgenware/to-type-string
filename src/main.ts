31eda9224e3a2aecb7832acb9b310ff7534be035
Mgen <m@mgenware.com>
2020-10-16 06:05:50 +0800

    Wrap everything in a try-catch

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 // tslint:disable-next-line no-any
 export default function toTypeString(obj: any): string {
-  if (obj === null) {
-    return 'null';
-  }
-  const type = typeof obj;
-  if (type === 'object') {
-    if (Array.isArray(obj)) {
-      return 'Array';
+  try {
+    if (obj === null) {
+      return 'null';
     }
-    if (obj.constructor && obj.constructor.name) {
-      return obj.constructor.name;
+    const type = typeof obj;
+    if (type === 'object') {
+      if (Array.isArray(obj)) {
+        return 'Array';
+      }
+      if (obj.constructor && obj.constructor.name) {
+        return obj.constructor.name;
+      }
     }
+    return type;
+  } catch (err) {
+    // tslint:disable-next-line no-console
+    console.error(`to-type-string error: ${err.message}`);
+    return `${obj}`;
   }
-  return type;
 }
